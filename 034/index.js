7b8855647936558ae43528ecd7aa600ab95cf521
tsungtingdu <tsungtingdu@gmail.com>
2020-07-22 12:11:05 +0800

    complete layout

---
+++ 034/index.js
@@ -0,0 +1,24 @@
+// target the input element
+const input = document.getElementById("action__input_1");
+// click handler
+const toggleMenu = event => {
+  // target the menu element
+  const menu = document.getElementById("action__menu_1");
+  menu.classList.toggle("hidden");
+};
+// bind the event
+input.addEventListener("click", toggleMenu);
+
+// dark mode
+// target the switch element
+const darkModeToggle = document.getElementById("dark__mode__toggle");
+// toggle handler
+const darkModeToggleHandler = event => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
+// bind the event
+darkModeToggle.addEventListener("change", darkModeToggleHandler);
